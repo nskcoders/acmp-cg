1cac33878fd1edaffd20e65352cd750a41b5460f
exp101t <rittergerman2000@yandex.ru>
2017-06-05 18:15:53 +0700

    Update 0131.cpp

--- 0131.cpp
+++ 0131.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 main(){
-    int v, s, n, V = 0, N = 0, i = 1;
-    std::cin >> n;
-    while(std::cin >> v >> s && i++)
-        if(s && v > V) N = i, V = v;
-    std::cout << N - 1;
+    int v, s, V = 0, N = -1, i = 1;
+    std::cin >> s;
+    for(;std::cin >> v >> s; i++)
+        if(s & v > V) N = i, V = v;
+    std::cout << N;
 }
