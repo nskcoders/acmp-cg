3e63559e404a2a7612fffb7e7fa2e066079460f9
exp101t <rittergerman2000@yandex.ru>
2017-06-05 19:35:11 +0700

    Update 0131.cpp

--- 0131.cpp
+++ 0131.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #define o std::cin >> v
 
-int v, s, V, N = -1, i = 1;
+int v, s, V, N, i = 2;
 main(){
     o;
     for(; o >> s; i++) s & v > V ? N = i, V = v : 0;
-    std::cout << N;
+    std::cout << N - 1;
 }
