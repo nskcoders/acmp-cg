4827c674ff3ab3c5e0b5e2f395aaf340427c45bf
exp101t <rittergerman2000@yandex.ru>
2017-06-05 18:25:08 +0700

    Update 0131.cpp

--- 0131.cpp
+++ 0131.cpp
@@ -4,6 +4,6 @@ main(){
     int v, s, V = 0, N = -1, i = 1;
     std::cin >> s;
     for(;std::cin >> v >> s; i++)
-        if(s & v > V) N = i, V = v;
+        s & v > V ? N = i, V = v : 0;
     std::cout << N;
 }
