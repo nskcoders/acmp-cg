28240cb2e363c7d1fe20940135babd9b2ffb695f
exp101t <rittergerman2000@yandex.ru>
2017-06-05 18:53:50 +0700

    Update 0131.cpp

--- 0131.cpp
+++ 0131.cpp
@@ -2,8 +2,7 @@
 #define o std::cin >> v
 
 main(){
-    int v, s, V = 0, N = -1, i = 1;
-    o;
+    int v, s, V = 0, N = -1, i = 1; o;
     for(; o >> s; i++)
         s & v > V ? N = i, V = v : 0;
     std::cout << N;
