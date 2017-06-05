d93af6774b0ec7da06ead43fff8c2e69bfba6b72
exp101t <rittergerman2000@yandex.ru>
2017-06-05 21:53:38 +0700

    Update 0284.cpp

--- 0284.cpp
+++ 0284.cpp
@@ -2,10 +2,10 @@
 #define x std::cin  >>
 #define y std::cout <<
 
-int n, i, j, a[1002];
+int i, j, a[1002];
 main(){
-    x n;
-    while(n + 1 - i++) x a[i];
+    x j;
+    while(j + 1 - i++) x a[i];
 
     while(x i >> j){
         while(j + 1 - i++) y a[i - 1] << ' ';
