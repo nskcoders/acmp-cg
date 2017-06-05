7c288d52e5c9904db4d5b27d22b3ccbcb6f2df36
Sergey Gorbunov <eaglemango@gmail.com>
2017-06-05 16:59:20 +0300

    Add files via upload

---
+++ 0284.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, i, j;
+int main() {
+    cin >> n;
+    int a[n];
+    for (; i < n; i++) cin >> a[i];
+
+    cin >> m;
+    while (m--) {
+        cin >> i >> j;
+        for (int k = i-1; k < j; k++) cout << ' ' << a[k];
+        cout << "\n";
+    }
+}
